3a550b5b18f0bc99a7066e19b5988329b9cd0f44
Patrick Gan <patrickgan2005@gmail.com>
2026-07-02 06:24:02 -0700

    Add dynamic journal entry display.
    Clicking a name from the name selection will change the journal entries displayed
    to entries associated with the character.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let arane = [
     {
-        "title": "Move-In",
-        "day": 1,
-        "body": "Today, I moved into Portgrand City!"
+        "title": "Cooking Date",
+        "day": 31,
+        "body": "Today, Arane came over to my house for our cooking date."
     },
     {
         "title": "Dinner at Arane's",
@@ -10,22 +10,49 @@ let arane = [
         "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira."
     },
     {
-        "title": "Cooking with Arane",
-        "day": 31,
-        "body": "Today, Arane came over to my house for our cooking date."
+        "title": "Move-In",
+        "day": 1,
+        "body": "Today, I moved into Portgrand City!"
+    }
+]
+
+let akira = [
+    {
+        "title": "Fun in the Sun",
+        "day": 40,
+        "body": "Today, I went to the beach with Akira."
+    },
+    {
+        "title": "Hospital",
+        "day": 13,
+        "body": "I went to the hospital to return Akira's umbrella, and got hit by a gurney!"
+    },
+    {
+        "title": "Dinner at Arane's",
+        "day": 7,
+        "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira."
     }
 ]
 
 const entriesMap = {
-    "arane": arane
+    "arane": arane,
+    "akira": akira
 }
 
 function displayEntries(id) {
-    const element = document.querySelector(".entries");
+    const entriesElement = document.querySelector(".entries");
+    const template = document.querySelector(".entries > li");
+    entriesElement.innerHTML = "";
     // TODO: implement fetch const entriesJson = fetch(`./entries/${id}.json`)
     const entries = entriesMap[id];
     for (const entry of entries) {
-        const temp = document.createElement('div');
+        const clone = template.cloneNode(true);
+        const title = entry["title"];
+        const day = entry["day"];
+        const body = entry["body"];
+        clone.querySelector(".entry-title").innerHTML = `Day ${day} -- ${title}`;
+        clone.querySelector(".entry-body").innerHTML = body;
+        entriesElement.appendChild(clone);
     }
 }
 
@@ -40,9 +67,11 @@ function onClickName(e) {
     const selected = document.querySelector(".selected");
     selected.classList.remove("selected");
     this.classList.add("selected");
+    // TODO: implement displayEntries
+    displayEntries(this.id);
 }
 
-function addEventListeners() {
+function onLoad() {
     const elements = document.querySelectorAll(".names > li");
     for (const element of elements) {
         element.addEventListener("click", onClickName);

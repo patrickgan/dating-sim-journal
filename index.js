8a0b41fcbf63b561f326833eaf3e1d1fae8e5b27
Patrick Gan <patrickgan2005@gmail.com>
2026-07-09 15:57:56 -0700

    Add entry character display.

--- index.js
+++ index.js
@@ -2,17 +2,28 @@ let arane = [
     {
         "title": "Cooking Date",
         "day": 31,
-        "body": "Today, Arane came over to my house for our cooking date."
+        "body": "Today, Arane came over to my house for our cooking date.",
+        "characters": [
+            "arane"
+        ]
     },
     {
         "title": "Dinner at Arane's",
         "day": 7,
-        "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira."
+        "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira.",
+        "characters": [
+            "arane",
+            "akira"
+        ]
     },
     {
         "title": "Move-In",
         "day": 1,
-        "body": "Today, I moved into Portgrand City!"
+        "body": "Today, I moved into Portgrand City!",
+        "characters": [
+            "arane",
+            "neruko"
+        ]
     }
 ]
 
@@ -20,23 +31,39 @@ let akira = [
     {
         "title": "Fun in the Sun",
         "day": 40,
-        "body": "Today, I went to the beach with Akira."
+        "body": "Today, I went to the beach with Akira.",
+        "characters": [
+            "akira"
+        ]
     },
     {
         "title": "Hospital",
         "day": 13,
-        "body": "I went to the hospital to return Akira's umbrella, and got hit by a gurney!"
+        "body": "I went to the hospital to return Akira's umbrella, and got hit by a gurney!",
+        "characters": [
+            "akira"
+        ]
     },
     {
         "title": "Dinner at Arane's",
         "day": 7,
-        "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira."
+        "body": "I was finally able to join Arane for dinner at her place. We were joined by her mother, Akira.",
+        "characters": [
+            "arane",
+            "akira"
+        ]
     }
 ]
 
 const entriesMap = {
     "arane": arane,
     "akira": akira
+}
+
+const charactersMap = {
+    "arane": "purple",
+    "akira": "pink",
+    "neruko": "lightblue",
 }
 
 function displayEntries(id) {
@@ -50,7 +77,15 @@ function displayEntries(id) {
         const title = entry["title"];
         const day = entry["day"];
         const body = entry["body"];
+        const characters = entry["characters"];
         clone.querySelector(".entry-title").innerHTML = `Day ${day} -- ${title}`;
+        clone.querySelector(".characters-list").innerHTML = "";
+        for (const character of characters) {
+            const characterNode = document.createElement("div");
+            characterNode.style.backgroundColor = charactersMap[character];
+            characterNode.className = "entry-character";
+            clone.querySelector(".characters-list").appendChild(characterNode);
+        }
         clone.querySelector(".entry-body").innerHTML = body;
         entriesElement.appendChild(clone);
     }
@@ -75,7 +110,9 @@ function onLoad() {
     for (const element of elements) {
         element.addEventListener("click", onClickName);
     }
+
     const selected = document.querySelector(".selected");
     displayEntries(selected.id);
+
     console.log("Done.");
 }
